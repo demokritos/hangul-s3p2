e6bc85eef48b7df44bdbca068ff56463221b00f2
Taegil Bae <esrevinu@gmail.com>
2019-05-28 07:31:09 +0900

    Implement symbol extensions

--- hangul-s3p2.el
+++ hangul-s3p2.el
@@ -67,7 +67,46 @@
        [?ㅈ ?ㅗ] [?ㄹ ?ㅑ] [?ㅆ ?ㅛ] ?ㄹ [?ㅁ ?ㆍ]
        ?{ ?| ?} ?~ ])
 
+;; ! " # $ % & ' ( ) * + , - . /
+;; 0 1 2 3 4 5 6 7 8 9
+;; : ; < = > ? @
+;; A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+;; [ \ ] ^ _ `
+;; a b c d e f g h i j k l m n o p q r s t u v w x y z
+;; { | } ~
+
+;; unassinged ones: `"' -> `㎖', `{' -> `ℓ', `}' -> `㎕'
+(defconst hangul-s3p2-symbol-1-keymap
+  [ ?Ⅰ ?㎖ ?Ⅲ ?Ⅳ ?Ⅴ ?Ⅶ ?㉫ ?Ⅸ ?Ⅹ ?Ⅷ ?Ⅻ ?、 ?± ?。 ?㉪
+        ?§ ?µ ?㎡ ?㎥ ?￦ ?￥ ?Ω ?￡ ?€ ?￠
+        ?´ ?㉥ ?≤ ?≠ ?≥ ?√ ?Ⅱ
+        ?α ?β ?χ ?δ ?ε ?φ ?γ ?η ?ι ?◌ ?κ ?λ ?μ ?ν ?ο ?π ?θ ?ρ ?σ ?τ ?υ ?⋅ ?ω ?ξ ?ψ ?ζ
+        ?【 ?≒ ?】 ?Ⅵ ?Ⅺ ?™
+        ?◇ ?÷ ?° ?○ ?△ ?· ?… ?㉡ ?㉤ ?㉧ ?㉠ ?㉨ ?㉭ ?㉦ ?㉩ ?㉬ ?◁ ?▷ ?□ ?ː ?㉢ ?× ?▽ ?′ ?㉣ ?″
+        ?ℓ ?∣ ?㎕ ?∼ ])
+
+;; unassinged ones: `"' -> `∟' (right angle)
+(defconst hangul-s3p2-symbol-2-keymap
+  [ ?₁ ?∟ ?₃ ?₄ ?₅ ?₇ ?♪ ?₉ ?₀ ?₈ ?₊ ?〈 ?♂ ?〉 ?※
+        ?⑩ ?① ?② ?③ ?④ ?⑤ ?⑥ ?⑦ ?⑧ ?⑨
+        ?↳ ?↘ ?⊂ ?⊃ ?⤷ ?₂
+        ?∀ ?≡ ?↛ ?∆ ?∃ ?ϕ ?Γ ?☐ ?∩ ?∨ ?∧ ?↲ ?⊇ ?⊆ ?↰ ?↱ ?∈ ?¬ ?∑ ?⊤ ?∪ ?∘ ?∋ ?↮ ?⊢ ?↚
+        ?〔 ?¶ ?〕 ?₆ ?₋ ?©
+        ?◈ ?‰ ?☎ ?◉ ?↑ ?◦ ?― ?〃 ?℃ ?‘ ?’ ?↙ ?\」 ?\「 ?↖ ?↗ ?← ?→ ?▣ ?↔ ?㈜ ?✕ ?↓ ?☆ ?↕ ?♡
+        ?₍ ?∥ ?₎ ?≈ ])
+
+;; unassinged ones: `"' -> `∮' (contour integral)
+(defconst hangul-s3p2-symbol-3-keymap
+  [ ?¹ ?∮ ?³ ?⁴ ?⁵ ?⁷ ?♬ ?⁹ ?⁰ ?⁸ ?⁺ ?《 ?☂ ?》 ?☠
+        ?⑳ ?⑪ ?⑫ ?⑬ ?⑭ ?⑮ ?⑯ ?⑰ ?⑱ ?⑲
+        ?⬎ ?⇘ ?⊄ ?☃ ?⊅ ?⤶ ?²
+        ?Å ?≅ ?⊗ ?∇ ?∄ ?∅ ?∫ ?☑ ?∞ ?∵ ?∴ ?⬐ ?⊋ ?⊊ ?⬑ ?⬏ ?∉ ?∂ ?∏ ?⊥ ?∝ ?∙ ?∌ ?⊕ ?⊨ ?∠
+        ?☀ ?¦ ?☁ ?⁶ ?⁻ ?®
+        ?◆ ?‱ ?♨ ?● ?⇑ ?• ?￣ ?✓ ?℉ ?“ ?” ?⇙ ?\』 ?\『 ?⇖ ?⇗ ?⇐ ?⇒ ?■ ?⇔ ?㉾ ?✂ ?⇓ ?★ ?⇕ ?♥
+        ?⁽ ?∦ ?⁾ ?∽ ])
+
 (defvar hangul-gyeob-mo nil)
+(defvar hangul-s3p2-symbol nil)
 
 (defsubst jamo-offset (char)
   (- char ?ㄱ -1))
@@ -93,21 +132,34 @@
       ;; not vector
       (if (and (>= char ?ㅏ) (<= char ?ㅣ))
           (hangul3-input-method-jung (jamo-offset char))
-        ;; (if (and (zerop (aref hangul-queue 2)) (zerop (aref hangul-queue 4))
-        ;;       (eq (aref hangul-queue 0) ?ㅇ) (not (vectorp char))
-        ;;       (memq char '(?ㄱ ?ㅈ ?ㅂ)))
-        ;;  ;; TODO not implemented
-        ;;  )
         (if (and (>= char ?ㄱ) (<= char ?ㅎ))
-            (if (and (notzerop (aref hangul-queue 0))
-                     (zerop (aref hangul-queue 2))
-                     (setq hangul-gyeob-mo (cdr (assq char '((?ㅋ . ?ㅗ) (?ㅊ . ?ㅜ)
-                                                             (?ㅁ . ?ㅡ) (?ㅍ . ?ㆍ))))))
-                (hangul3-input-method-jung (jamo-offset hangul-gyeob-mo))
-              (hangul3-input-method-cho (jamo-offset char)))
+            (if (and (zerop (aref hangul-queue 2)) (zerop (aref hangul-queue 4))
+                     (eq (aref hangul-queue 0) (jamo-offset ?ㅇ))
+                     (memq char '(?ㄱ ?ㅈ ?ㅂ)))
+                (setq hangul-s3p2-symbol char)
+              (if (and (notzerop (aref hangul-queue 0))
+                       (zerop (aref hangul-queue 2))
+                       (setq hangul-gyeob-mo (cdr (assq char '((?ㅋ . ?ㅗ) (?ㅊ . ?ㅜ)
+                                                               (?ㅁ . ?ㅡ) (?ㅍ . ?ㆍ))))))
+                  (hangul3-input-method-jung (jamo-offset hangul-gyeob-mo))
+                (hangul3-input-method-cho (jamo-offset char))))
            (setq hangul-queue (make-vector 6 0))
            (insert (decode-char 'ucs char))
            (move-overlay quail-overlay (point) (point)))))))
+
+(defun hangul-s3p2-symbol-input-method-internal (key)
+  (let (char)
+    (cond ((eq hangul-s3p2-symbol ?ㄱ)
+           (setq char (aref hangul-s3p2-symbol-1-keymap (- key 33))))
+          ((eq hangul-s3p2-symbol ?ㅈ)
+           (setq char (aref hangul-s3p2-symbol-2-keymap (- key 33))))
+          ((eq hangul-s3p2-symbol ?ㅂ)
+           (setq char (aref hangul-s3p2-symbol-2-keymap (- key 33)))))
+    (setq hangul-s3p2-symbol nil)
+    (setq hangul-queue (make-vector 6 0))
+    (quail-delete-region)
+    (insert (decode-char 'ucs char))
+    (move-overlay quail-overlay (point) (point))))
 
 (defun hangul-s3p2-input-method (key)
   "Shin Se-beol input method."
@@ -129,7 +181,9 @@
                             (= 1 (length seq))
                             (setq key (aref seq 0))
                             (and (>= key 33) (< key 127)))
-                       (hangul-s3p2-input-method-internal key))
+                       (if hangul-s3p2-symbol
+                           (hangul-s3p2-symbol-input-method-internal key)
+                         (hangul-s3p2-input-method-internal key)))
                       ((commandp cmd)
                        (call-interactively cmd))
                       (t
